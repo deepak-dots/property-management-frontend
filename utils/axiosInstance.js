49b38725cd68d3ae8adace1937fca6f6ce6c3581
deepak-dots <87978637+deepak-dots@users.noreply.github.com>
2025-08-14 18:59:29 +0530

    Update axiosInstance.js

--- utils/axiosInstance.js
+++ utils/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api', 
+  baseURL: 'https://property-management-backend-hrjp.onrender.com/api', 
   //baseURL: 'http://192.168.1.50:3000/api', // Use PC IP + backend port
   withCredentials: true,
 });
