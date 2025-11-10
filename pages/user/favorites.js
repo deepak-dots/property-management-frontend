86916f317cd1dcc47088b26180b5fd118c7fb91e
admin <deepak.k@dotsquares.com>
2025-11-10 16:36:26 +0530

    update the grid layout

--- pages/user/favorites.js
+++ pages/user/favorites.js
@@ -53,7 +53,7 @@ export default function MyFavorites() {
         ) : favoriteList.length === 0 ? (
           <p>You have no favorite properties yet.</p>
         ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
             {favoriteList.map((property) => (
               <PropertyCard
                 key={property._id}
