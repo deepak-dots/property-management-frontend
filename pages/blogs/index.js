91a62ceedfe2749d8d2201c44e7dd6ea90a08850
admin <deepak.k@dotsquares.com>
2025-11-10 16:31:30 +0530

    update the grid column 3 to 4

--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -24,7 +24,7 @@ export default function BlogsPage() {
           Blog
         </h2>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
           {loading ? (
             <p className="text-center text-gray-500 col-span-full">Loading...</p>
           ) : posts.length === 0 ? (
